f3674f86cb2a52b6656bfbc1b469afe0775bd6ee
yaodebian <2801540120@qq.com>
2018-12-03 19:25:49 +0800

    Promise原理探究：.then方法

---
+++ ES6_sourcecode_analyse/Promise_Code_By_Js/then_code.js
@@ -0,0 +1,65 @@
+  // 添加then方法
+  then(onFulfilled, onRejected) {
+    const {
+      _value,
+      _status
+    } = this
+    // 返回一个新的Promise对象
+    return new MyPromise((onFulfilledNext, onRejectedNext) => {
+      // 封装一个成功时执行的函数
+      let fulfilled = value => {
+        try {
+          if (!isFunction(onFulfilled)) {
+            onFulfilledNext(value)
+          } else {
+            let res = onFulfilled(value);
+            if (res instanceof MyPromise) {
+              // 如果当前回调函数返回MyPromise对象，必须等待其状态改变后在执行下一个回调
+              res.then(onFulfilledNext, onRejectedNext)
+            } else {
+              //否则会将返回结果直接作为参数，传入下一个then的回调函数，并立即执行下一个then的回调函数
+              onFulfilledNext(res)
+            }
+          }
+        } catch (err) {
+          // 如果函数执行出错，新的Promise对象的状态为失败
+          onRejectedNext(err)
+        }
+      }
+      // 封装一个失败时执行的函数
+      let rejected = error => {
+        try {
+          if (!isFunction(onRejected)) {
+            onRejectedNext(error)
+          } else {
+            let res = onRejected(error);
+            if (res instanceof MyPromise) {
+              // 如果当前回调函数返回MyPromise对象，必须等待其状态改变后在执行下一个回调
+              res.then(onFulfilledNext, onRejectedNext)
+            } else {
+              //否则会将返回结果直接作为参数，传入下一个then的回调函数，并立即执行下一个then的回调函数
+              onFulfilledNext(res)
+            }
+          }
+        } catch (err) {
+          // 如果函数执行出错，新的Promise对象的状态为失败
+          onRejectedNext(err)
+        }
+      }
+      // 通过当前作用域下的fulfilled和rejected进行状态改变
+      switch (_status) {
+        // 当状态为pending时，将then方法回调函数假如执行队列等待执行
+        case PENDING:
+          this._fulfilledQueues.push(fulfilled)
+          this._rejectedQueues.push(rejected)
+          break
+          // 当状态已经改变时，立即执行对应的回调函数
+        case FULFILLED:
+          fulfilled(_value)
+          break
+        case REJECTED:
+          rejected(_value)
+          break
+      }
+    })
+  }
