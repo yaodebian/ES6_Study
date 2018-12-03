3912325c1aa4e9cb49f6675081ecc61042a00e77
yaodebian <2801540120@qq.com>
2018-12-03 19:00:24 +0800

    创建ES6原理探究目录，并创建Promise原理分析文件夹以及新建Promise封装版本1

---
+++ ES6_sourcecode_analyse/Promise_Code_By_Js/demo1.js
@@ -0,0 +1,37 @@
+// 判断变量是否为函数
+const isFunction = variable => typeof variable === 'function'
+
+// 定义Promise的三种状态
+const PENDING = 'PENDING'
+const FULFILLED = 'FULFILLED'
+const REJECTED = 'REJECTED'
+
+class MyPromise {
+  constructor (handle) {
+    if (!isFunction(handle)) {
+      throw new Error('MyPromise must accept a function as a parameter')
+    }
+    // 添加状态
+    this._status = PENDING
+    // 添加状态值
+    this._value = undefined
+    // 执行handle
+    try {
+      handle(this._resolve.bind(this), this._reject.bind(this))
+    } catch (err) {
+      this._reject(err)
+    }
+  }
+  // 添加resolve时执行的函数
+  _resolve (val) {
+    if (this._status !== PENDING) return
+    this._status = FULFILLED
+    this._value = val
+  }
+  // 添加reject时执行的函数
+  _reject (err) {
+    if (this._status !== PENDING) return
+    this._status = REJECTED
+    this._value = err
+  }
+}
